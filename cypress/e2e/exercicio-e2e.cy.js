3cc510480bd07ce0fbc7d1de549db3f421856fb3
Cláudio Santana <claudio_bs@icloud.com>
2023-09-19 11:30:17 -0300

    Projeto automação de teste UI finalizado.

--- cypress/e2e/exercicio-e2e.cy.js
+++ cypress/e2e/exercicio-e2e.cy.js
@@ -1,4 +1,6 @@
 /// <reference types="cypress" />
+import FaturamentoPage from "../support/page-objects/faturamento.page";
+const dadosFaturamento = require('../fixtures/faturamento.json')
 
 context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
     /*  Como cliente 
@@ -9,13 +11,46 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
         Preenchendo todas opções no checkout
         E validando minha compra ao final */
 
-    beforeEach(() => {
-        cy.visit('/')
+    before(() => {
+        cy.visit('produtos')
+    });
+
+
+    afterEach(() => {
+        cy.screenshot()
     });
 
     it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
-        //TODO 
-    });
+        cy.addProdutos1('Abominable Hoodie', 'L', 'Blue', 2)
+        
+        cy.addProdutos2('Erikssen CoolTech™ Fitness Tank', 'L', 'Red', 5)
+        
+        cy.addProdutos3('Frankie Sweatshirt', 'L', 'White', 3)
 
+        cy.addProdutos4('Hawkeye Yoga Short', '36', 'Black', 10)
+
+        cy.get('.woocommerce-message > .button').click()
+
+        cy.get('.checkout-button').click()
+
+        FaturamentoPage.editarEnderecoFaturamento(
+            dadosFaturamento[1].nome,
+            dadosFaturamento[1].sobrenome,
+            dadosFaturamento[1].empresa,
+            dadosFaturamento[1].pais,
+            dadosFaturamento[1].endereco,
+            dadosFaturamento[1].complemento,
+            dadosFaturamento[1].cidade,
+            dadosFaturamento[1].estado,
+            dadosFaturamento[1].cep,
+            dadosFaturamento[1].telefone,
+            dadosFaturamento[1].email,
+            dadosFaturamento[1].observacao
+        )
+
+        cy.get('#place_order').click()
+
+        cy.get('.woocommerce-notice').contains('Obrigado. Seu pedido foi recebido.')
+    });
 
 })
